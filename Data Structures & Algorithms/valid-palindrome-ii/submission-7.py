8055d0bfe03ca2f217c0657920794cca96e1abb8
Annapurna <54123673+amandalika19@users.noreply.github.com>
2026-04-30 21:23:19 -0700

    Add: valid-palindrome-ii - submission-7

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        l, r = 0, len(s) - 1
+
+        while l < r:
+
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+            else:
+                skipL = s[l + 1: r + 1]
+                skipR = s[l : r]
+            
+                if skipL == skipL[::-1] or skipR == skipR[::-1]:
+                    return True
+                else:
+                    return False
+        return True
