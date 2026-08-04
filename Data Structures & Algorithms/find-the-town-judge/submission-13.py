754181c238df31054a5eef67f90fd3cf9c4840ea
Annapurna <54123673+amandalika19@users.noreply.github.com>
2026-08-03 19:24:48 -0700

    Add: find-the-town-judge - submission-13

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-13.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        out = defaultdict(int)
+        inc = defaultdict(int)
+
+        for s, d in trust:
+            out[s] += 1
+            inc[d] += 1
+    
+        for i in range(0, n + 1):
+            if out[i] == 0 and inc[i] == n-1:
+                return i
+        
+        return -1
